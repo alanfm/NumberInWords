6ee061ed3cfa1aff1f491013f30ac6dac2b7fe08
Alan Freire <alan_freire@msn.com>
2016-09-14 16:47:58 -0300

    Inclusão de comentários no arquivo extensao

--- extensao.php
+++ extensao.php
@@ -1,8 +1,13 @@
 <?php
 
-include ('classes/Numeros.php');
+// Inclui o arquivo da classe Numeros
+require ('classes/Numeros.php');
 
+// Cria uma instancia de Números
 $n = new Numeros(filter_input(INPUT_POST, 'valor', FILTER_SANITIZE_NUMBER_INT));
 
+// Seta o cabeçalho para saída do tipo JSON
 header('Content-Type: application/json');
-echo (ucfirst($n->porExtenso()) . '.');
+
+// Imprime o número passado por extenso
+echo $n->porExtenso(), '.';
